d272824a6aecfac1b0b89480721766d6627d8282
shaimaamagdy31 <shimaaaamadgy900@gmail.com>
2026-05-14 14:54:53 +0200

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,8 +10,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'movies app',
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (cotext , child){
+        return MaterialApp(
+          title: 'movies app',
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
